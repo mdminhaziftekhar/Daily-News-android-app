09afd82677bc2cba5ad6eb87d13f210b603b5181
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-06-09 23:07:25 +0600

    Added JavaScript support

--- app/src/main/java/com/dailynews/webViewController.java
+++ app/src/main/java/com/dailynews/webViewController.java
@@ -1,5 +1,6 @@
 package com.dailynews;
 
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -8,6 +9,18 @@ public class webViewController extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
         view.loadUrl(url);
+
+        //These lines are for inabeling javascript
+        WebSettings webSettings = view.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+
         return true;
     }
 }
